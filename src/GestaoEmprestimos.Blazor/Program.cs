35064ff732953f8283bae639b93f2ce73a159e20
Maisa Vendrame Kuhne <kuhnemaisa@gmail.com>
2026-05-30 18:51:38 -0300

    criamos a pagina de materias no blazor

--- src/GestaoEmprestimos.Blazor/Program.cs
+++ src/GestaoEmprestimos.Blazor/Program.cs
@@ -1,26 +1,26 @@
 using GestaoEmprestimos.Blazor.Components;
+using GestaoEmprestimos.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// REGISTRO DO SERVIÇO COMO SINGLETON (Persistência em Memória)
+builder.Services.AddSingleton<EmprestimoService>();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
